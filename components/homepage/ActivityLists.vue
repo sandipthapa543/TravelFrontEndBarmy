c327d097852868ce41eaef987389a40c84bff4d6
Samyam Dabadi <samyamdw@gmail.com>
2020-07-09 14:26:21 +0545

    Activity data fetched from API

---
+++ components/homepage/ActivityLists.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-row>
+    <v-col
+      v-for="activity in activities"
+      :key="activity.id"
+      cols="12"
+      sm="6"
+      md="3"
+    >
+      <v-card>
+        <v-img
+          :src="`http://localhost:8080/uploads/activities/` + activity.Image"
+          class="white--text align-end"
+          gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+          height="200px"
+        >
+          <div class="ts">
+            <v-card-title
+              class="pt-0 pointer"
+              v-text="activity.Activity_Name"
+              @click="$router.push('/activity/' + activity.Slug)"
+            ></v-card-title>
+            <v-card-subtitle
+              class="pb-1"
+              style="color: white !important"
+              v-text="
+                activity.packages.length === 1
+                  ? activity.packages.length + ` Package`
+                  : activity.packages.length + ` Packages`
+              "
+            ></v-card-subtitle>
+          </div>
+        </v-img>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: {
+    activities: {}
+  },
+  // data() {}
+};
+</script>
+
+<style>
+.ts {
+  text-shadow: 2px 2px #000;
+  background: linear-gradient(180deg, transparent 0, rgba(0, 0, 0, 0.75));
+}
+</style>
