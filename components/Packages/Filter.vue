3ccd7baf1ad816fb97b2f71f9ed91e6e58528c22
Samyam Dabadi <samyamdw@gmail.com>
2020-07-26 20:46:58 +0545

    Package sorting

---
+++ components/Packages/Filter.vue
@@ -0,0 +1,36 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  data: () => ({
+    queries: "id",
+    choices: [
+      {
+        name: "Popularity",
+        value: "id"
+      },
+      {
+        name: "Price",
+        value: "Price"
+      },
+      {
+        name: "Duration",
+        value: "Days"
+      },
+      {
+        name: "Difficulty_level",
+        value: "Difficulty_level"
+      }
+    ]
+  }),
+  methods: {
+  },
+  updated(){
+      this.$emit("sorting", this.queries);
+  }
+};
+</script>
+
+<style></style>
